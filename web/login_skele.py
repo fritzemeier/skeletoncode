b8d51cbc9bd1fb21f62fea8e7007c34268383a4c
fritzemeier <austinfritzemeierdsu@gmail.com>
2020-01-29 13:41:18 -0600

    user stuff

--- web/login_skele.py
+++ web/login_skele.py
@@ -2,27 +2,51 @@ import sys,os
 import requests
 
 def parse_args(INFO):
+
 	for i in range(1,len(sys.argv)):
+
 		CURR = sys.argv[i]
 		KEY = CURR.split("=")[0]
 		VAL = CURR.strip(KEY+"=")
+
 		if KEY in INFO.keys():
 			INFO[KEY] = VAL
 
-		return INFO
+	return INFO
+
+def print_dict(DICT):
+	for i in DICT.keys():
+		print(i+": "+DICT[i])
 
 def main():
 	requestInfo = {
 		"URL":"",
 		"HEADERS":"",
-		"DATA":"",
+		"DATA":""
+	}
+
+	userData = {
+
+		"USER":"",
+		"PASS":"",
+		"USERFILE":"",
+		"PASSFILE":""
+
 	}
 
 	if len(sys.argv) > 1:
 		requestInfo = parse_args(requestInfo)
+		userData = parse_args(userData)
 
-	for i in requestInfo.keys():
-		print(i+": "+requestInfo[i])
+	if not all(VAL == "" for VAL in requestInfo.values()):
+		print("Request Information")
+		print_dict(requestInfo)
+		print()
+
+	if not all(VAL == "" for VAL in userData.values()):
+		print("User Data")
+		print_dict(userData)
+		print()
 
 if __name__ == "__main__":
 	main()
