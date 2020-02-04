dc9ef396d013d2100e116da83af458f4129ab561
fritzemeier <austinfritzemeierdsu@gmail.com>
2020-02-04 16:57:11 -0600

    i think it works

--- atf_deobf.py
+++ atf_deobf.py
@@ -1,5 +1,4 @@
 import sys
-#import collections
 from collections import OrderedDict
 
 def parse_args(INFO):
@@ -60,13 +59,15 @@ def main():
 		LO = i[(len(SFILE)+1):]
 		FUNC = LO.replace(" ","").split("=")[0]
 		PCS = LO.replace(" ","").replace("\n","").split("=")[1].split("+")
-
 		obfStrs[FUNC] = { \
 					"SFILE":SFILE, \
-					"PCS":PCS, \
+					"PCS":OrderedDict(), \
 					"ALG": OrderedDict(), \
 					"FULLSTR":""
 				}
+
+		for z in LO.replace(" ","").replace("\n","").split("=")[1].split("+"):
+			obfStrs[FUNC]["PCS"][z] = z
 
 	deobfStrs = {}
 
@@ -82,13 +83,11 @@ def main():
 
 			BEGIN = LINE.replace(" ","").split("=")[0]
 
-			if BEGIN in obfStrs[KEY]["PCS"]:
+			if BEGIN in obfStrs[KEY]["PCS"].keys():
 
 				OSTR = LINE.replace(" ","").replace("\n","").replace(")","").split("(")[1].split(",")[0]
 				ENTRY = int(LINE.replace(" ","").replace("\n","").replace(")","").split("(")[1].split(",")[1])
 				LEN = int(LINE.replace(" ","").replace("\n","").replace(")","").split("(")[1].split(",")[2])
-
-#				print("OSTR: "+OSTR+"\nENTRY: "+str(ENTRY)+"\nLEN: "+str(LEN))
 
 				obfStrs[KEY]["ALG"][BEGIN] = { \
 								"OSTR":OSTR, \
@@ -110,100 +109,24 @@ def main():
 					ENTRY = obfStrs[KEY]["ALG"][PC1]["ENTRY"]
 					LEN = obfStrs[KEY]["ALG"][PC1]["LEN"]
 
-					FOUND_STR = CHUNK.replace('"','').split("=")[1][ENTRY:(ENTRY+LEN+1)]
+					FOUND_STR = CHUNK.replace('"','').split("=")[1][ENTRY:(ENTRY+LEN)]
 					obfStrs[KEY]["ALG"][PC1]["STR"] = FOUND_STR
-					obfStrs[KEY]["FULLSTR"] += FOUND_STR
-
-	for KEY in obfStrs.keys():
-		print("============================="+KEY)
-
-		for PC in obfStrs[KEY]["ALG"].keys():
-			print("++++++++++++++++++++++"+PC)
-
-			print(obfStrs[KEY]["ALG"][PC]["STR"])
-#		print(obfStrs[KEY]["FULLSTR"])
-
-	sys.exit()
 
 	for KEY in obfStrs.keys():
 
-		print("KEY: "+KEY)
+		print("Obfuscated Variable: "+KEY)
+		print("Source File: "+obfStrs[KEY]["SFILE"])
+		print("De-obfuscated String\n------------------------------------------------")
 
-		for SPLIT in files[obfStrs[KEY]["SFILE"]].split("\n"):
-			if obfStrs[KEY]["OSTR"] in SPLIT:
-				print(SPLIT)
+		for PC in obfStrs[KEY]["PCS"].keys():
 
-#	print(str(files))
+			if PC in obfStrs.keys() or PC[:4] == "Chr(":
+				obfStrs[KEY]["FULLSTR"] += "<<"+PC+">>    "
+				continue
+			obfStrs[KEY]["FULLSTR"] += obfStrs[KEY]["ALG"][PC]["STR"]
 
-
-
-# ----------------------------------------------------------------------------------------------------
-#	obfStr = obfFile.readline().split("=")[1]
-#	obfArr = obfStr.replace(" ","").replace("\n","").split("+")
-
-#	print(obfArr)
-
-#	sys.exit()
-
-
-	obfDict = {}
-	strDict = {}
-
-	deobfStr = ""
-
-	for i in obfArr:
-		obfDict[i.replace("\n","")] = { \
-
-						"VAR":"", \
-						"P1":"", \
-						"P2":""
-
-					}
-
-	for i in inFile:
-		TMP = i.split("=")[0]
-
-		VAR = TMP.replace(" ","").replace("\n","")
-
-		STR =  i[(len(TMP)+1):-1]
-
-		if VAR in obfDict.keys() and not obfDict[VAR]["VAR"]: #and "Mid" not in STR:
-			nameVar, p1, p2 = STR.replace(" ","").split("(")[1].strip(")").split(",")
-			#obfDict[VAR] = STR
-
-			obfDict[VAR]["VAR"] = nameVar
-			obfDict[VAR]["P1"] = p1
-			obfDict[VAR]["P2"] = p2
-
-	print_dict(obfDict)
-
-	for i in obfDict.keys():
-		if obfDict[i]["VAR"]:
-			strDict[obfDict[i]["VAR"]] = ""
-
-	if cliArgs["IF"]:
-		inFile = open(cliArgs["IF"], "r")
-
-	for i in inFile:
-		TMP = i.split("=")[0]
-
-		VAR = TMP.strip(" ")
-
-		STR =  i[(len(TMP)+2):-1]
-
-		if VAR in strDict.keys() and not strDict[VAR]:
-			strDict[VAR] = STR
-
-	print_dict(strDict)
-
-#	sys.exit()
-
-	for i in range(0,(len(obfArr))):
-		if obfArr[i] in obfDict.keys() and obfDict[obfArr[i]]["VAR"]:
-			deobfStr += strDict[obfDict[obfArr[i]]["VAR"]][(int(obfDict[obfArr[i]]["P1"])):((int(obfDict[obfArr[i]]["P1"])+int(obfDict[obfArr[i]]["P2"])))]
-		elif obfArr[i][:4] == "Chr(":
-			deobfStr += chr(int(obfArr[i][4:-1]))
-	print(deobfStr)
+		print(obfStrs[KEY]["FULLSTR"])
+		print("\n\n")
 
 if __name__ == "__main__":
 	main()
